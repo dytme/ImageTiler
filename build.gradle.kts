8479938fdd5ba05dc30a9edb7e7eb8e46d4d5536
dytme <dytme6@gmail.com>
2026-04-05 20:45:02 +0200

    Fixed PrettyVersion and LibraryName

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,13 +42,13 @@ version = if (project.hasProperty("githubReleaseTag")) {
 // Such as:
 // <libName>.jar will be the name of your build jar
 // <libName>.zip will be the name of your release file
-val libName = "ImageTilerP2D"
+val libName = "ImageTiler"
 
 // The group ID of your library, which uniquely identifies your project.
 // It's often written in reverse domain name notation.
 // For example, if your website is "myDomain.com", your group ID would be "com.myDomain".
 // Replace "com.myDomain" with your own domain or organization name.
-group = "com.dytme.ImageTilerP2D"
+group = "com.dytme.ImageTiler"
 
 // The location of your sketchbook folder. The sketchbook folder holds your installed
 // libraries, tools, and modes. It is needed if you:
